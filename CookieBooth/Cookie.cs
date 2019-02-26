027be238471a79c9e85501a975036b5041256871
Sarah Creasman <sarah.creasman@yahoo.com>
2019-02-25 19:35:14 -0500

    updated 2/25/19

---
+++ CookieBooth/Cookie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookieBooth
+{
+    public class Cookie
+    {
+        public string Name;
+        public double Cost;
+        public int InStock;
+
+        public Cookie(string name, double cost, int inStock)
+        {
+            Name = name;
+            Cost = cost;
+            InStock = inStock;
+        }
+
+
+    }
+}
---
+++ CookieBooth/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CookieBooth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadLines("cookieList.csv");
+
+            bool firstRecord = true;
+            foreach (var line in lines)
+            {
+                if (firstRecord == true)
+                {
+                    firstRecord = false;
+                }
+                else
+                {
+                    var fields = line.Split(',');
+                    var c = new Cookie(fields[0], double.Parse(fields[1]), int.Parse(fields[2]));
+                }
+            }
+
+
+
+        }
+    }
+}
