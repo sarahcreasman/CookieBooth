0f568218cd1487da5c01581c715522b073d7741e
Sarah Creasman <sarah.creasman@yahoo.com>
2019-03-25 19:39:14 -0400

    updated 3/25/19

--- CookieBooth/Cookie.cs
+++ CookieBooth/Cookie.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* Program Name: Cookie.cs
+ * Written By: Sarah Creasman
+ * Last Updated: March 25, 2019; 
+ * Purpose: Creates a cookie object to be used by the CookieBooth.cs app */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +11,11 @@ namespace CookieBooth
 {
     public class Cookie
     {
+        // Variables
         public string Name;
-        public int InStock;
+        public int InStock; // Cartons in stock
 
+        // Builds the object from information passed by Main Method
         public Cookie(string name, int inStock)
         {
             Name = name;
--- CookieBooth/Program.cs
+++ CookieBooth/Program.cs
@@ -1,6 +1,13 @@
-﻿using System;
+﻿/* Program Name: CookieBooth.cs
+ * Written By: Sarah Creasman
+ * Last Updated: March 25, 2019
+ * Purpose: To update the inventory of a girl scout troops stock of cookies for Cookie Booths. */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Text;
 
 namespace CookieBooth
 {
@@ -8,9 +15,11 @@ namespace CookieBooth
     {
         static void Main(string[] args)
         {
+            // Reads cookieList.csv and creates CookieList
             var lines = System.IO.File.ReadLines("cookieList.csv");
             var CookiesList = new List<Cookie>();
 
+            // Separates cookieList.csv into individual objects.
             foreach (var line in lines.Skip(1))
             {
                 var fields = line.Split(',');
@@ -19,22 +28,22 @@ namespace CookieBooth
                 CookiesList.Add(cookie);
             }
 
-            foreach (var cookie in CookiesList)
-            {
-                Console.WriteLine(cookie.Name + ", "  + cookie.InStock);
-            }
-
+            // Begins the user input and while loop in order to begin program
             Console.WriteLine("Would you like to update inventory? Enter Y or press any key to exit.");
             string userInput = Console.ReadLine();
+
+            // Creates writer to write new data
+            StreamWriter writer = new StreamWriter("cookieList.csv");
+            StringBuilder build = new StringBuilder();
 
             while (userInput.ToUpper() == "Y")
             {
                 Console.WriteLine("Would you like to add, remove, or view inventory? Enter Add, Remove, View, or press any key to exit");
                 string AddRemove = Console.ReadLine();
 
+                // Adds cookies to inventory
                 if (AddRemove.ToLower() == "add")
                 {
-                    // TODO: add cookies to inventory
                     Console.WriteLine("What cookie would you like to add?");
                     string cookieName = Console.ReadLine();
                     Console.WriteLine("How many cases would you like to add?");
@@ -42,17 +51,22 @@ namespace CookieBooth
 
                     foreach(Cookie cookie in CookiesList)
                     {
-                        if (cookie.Name.ToLower() == cookieName) {
+                        if (cookie.Name.ToLower().Contains(cookieName)) {
                             cookie.InStock += cookieCases;
                             Console.WriteLine(cookie.Name + " has " + cookie.InStock + " cases in stock.");
                         }
-                        else
-                        {
-                            Console.WriteLine("Cookie not found.");
-                        }
                     }
+
+                    // Writes new total to the csv file
+                    foreach(Cookie cookie in CookiesList)
+                    {
+                        var NewData = cookie.Name + "," + cookie.InStock;
+                        build.AppendLine(NewData);
+                    }
+                    writer.Write(build.ToString());
                 }
 
+                // Removes cookies from inventory
                 if (AddRemove.ToLower() == "remove")
                 {
                     Console.WriteLine("What cookie would you like to remove?");
@@ -62,18 +76,31 @@ namespace CookieBooth
 
                     foreach (Cookie cookie in CookiesList)
                     {
-                        if (cookie.Name.ToLower() == cookieName)
+                        if (cookie.Name.ToLower().Contains(cookieName))
                         {
-                            cookie.InStock -= cookieCases;
-                            Console.WriteLine(cookie.Name + " has " + cookie.InStock + " cases in stock.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Cookie not found.");
+                            if (cookieCases <= cookie.InStock)
+                            {
+                                cookie.InStock -= cookieCases;
+                                Console.WriteLine(cookie.Name + " has " + cookie.InStock + " cases in stock.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("You cannot take more cookies than you have in inventory!");
+                                Console.WriteLine(cookie.Name + " has " + cookie.InStock + " cases in stock.");
+                            }
                         }
                     }
+                    
+                    // Writes new total to CSV file
+                    foreach (Cookie cookie in CookiesList)
+                    {
+                        var NewData = cookie.Name + "," + cookie.InStock;
+                        build.AppendLine(NewData);
+                    }
+                    writer.Write(build.ToString());
                 }
 
+                // Views cookies in inventory
                 if (AddRemove.ToLower() == "view")
                 {
                     foreach (var cookie in CookiesList)
@@ -82,12 +109,15 @@ namespace CookieBooth
                     }
                 }
 
+                // Restarts loop with any other text entry
                 else
                 {
                     Console.WriteLine("Would you like to update inventory? Enter Y or press any key to exit.");
                     userInput = Console.ReadLine();
                 }
             }
+
+            writer.Close();
         }
     }
 }
